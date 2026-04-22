c6899958a807e20e080a6ca9797b0d0de90ebc98
bhuvanabhuraj23-byte <bhuvanabhuraj23@gmail.com>
2026-04-22 12:51:01 +0530

    hi

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def login():
 def about():
     return render_template("about.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "_main_":
+    app.run(debug=True,host="0.0.0.0",port=5000)
